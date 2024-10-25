23cfdfbee2ecacac727f3e9b6589418cd05eb5a1
nouhaila echchnaoui <nouhaila.chan2001@gmail.com>
2024-10-25 19:13:15 +0100

    attack func + space to attack

--- main.js
+++ main.js
@@ -236,6 +236,48 @@ scene ("fight", () => {
    });
 
    player1.onUpdate(() => resetAfterJump(player1));
+    function attack(player, excludedKeys) {
+      if (player.health === 0) {
+        return;
+      }
+
+      for (const key of excludedKeys) {
+        if (isKeyDown(key)) {
+          return;
+        }
+      }
+
+      const currentFlip = player.flipX;
+      if (player.curAnim() !== "attack") {
+        player.use(sprite(player.sprites.attack));
+        player.flipX = currentFlip;
+        const slashX = player.pos.x + 30;
+        const slashXFlipped = player.pos.x - 350;
+        const slashY = player.pos.y - 200;
+
+        add([
+          rect(300, 300),
+          area(),
+          pos(currentFlip ? slashXFlipped : slashX, slashY),
+          opacity(0),
+          player.id + "attackHitbox",
+        ]);
+
+        player.play("attack", {
+          onEnd: () => {
+            resetPlayerToIdle(player);
+            player.flipX = currentFlip;
+          },
+        });
+      }
+    }
+     onKeyPress("space", () => {
+       attack(player1, ["a", "d", "w"]);
+     });
+
+     onKeyRelease("space", () => {
+       destroyAll(player1.id + "attackHitbox");
+     });
 })
 
 go ("fight")
